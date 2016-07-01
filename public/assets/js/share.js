dd572e4bb481a8aa91c6785b2f351740b4928770
liam4 <towerofnix@gmail.com>
2016-06-30 22:08:54 -0300

    File selector frontend

--- public/assets/js/share.js
+++ public/assets/js/share.js
@@ -7,13 +7,31 @@
 
 const template = require('./template')
 
+function addResourceInput() {
+  const dialog = template('file-upload')
+  const fileInput = dialog.querySelector('.file-select-button')
+  fileInput.addEventListener('change', function(event) {
+    dialog.classList.add('has-file')
+    readFile(dialog, event).then(reader => {
+        console.log(reader.result)
+      })
+  })
+  document.getElementById('file-uploads').appendChild(dialog)
+}
+
+function readFile(dialog, event) {
+  return new Promise((resolve, reject) => {
+    const input = event.target
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
+      resolve(reader)
+    })
+    reader.readAsArrayBuffer(input.files[0])
+  })
+}
+
 module.exports = function() {
   // liam, work your page
-
-  function addResourceInput() {
-    const dialog = template('file-upload')
-    document.getElementById('file-uploads').appendChild(dialog)
-  }
 
   addResourceInput()
 
