1a4e3d259f32a359550570e4bc0abf3aa2295e6b
Alex Bates <16batesa@gmail.com>
2016-07-03 16:33:14 +0100

    add prj id env

--- server/main.js
+++ server/main.js
@@ -56,7 +56,7 @@ const db = require('../db')
 /////////////////////////////////////////////////////
 
 const badWords = '\\o(shtyl|(\\j*?)shpx(\\j*?)|s(h|i|\\*)?p?x(vat?)?|(\\j*?)fu(v|1|y)g(\\j*?)|pe(n|@|\\*)c(cre|crq|l)?|(onq|qhzo|wnpx)?(n|@)ff(u(b|0)yr|jvcr)?|(onq|qhzo|wnpx)?(n|@)efr(u(b|0)yr|jvcr)?|onfgneq|o(v|1|y|\\*)?g?pu(r?f)?|phag|phz|(tbq?)?qnz(a|z)(vg)?|qbhpur(\\j*?)|(arj)?snt(tbg|tng)?|sevt(tra|tva|tvat)?|bzst|cvff(\\j*?)|cbea|encr|ergneq|frk|f r k|fung|fyhg|gvg|ju(b|0)er(\\j*?)|jg(s|su|u))(f|rq)?\\o' // rot13
-const signupProjectId = 115307769 // null to disable check
+const signupProjectId = process.env.project_id || 115307769 // null to disable check
 const requireEmailConfirmedToShare = false
 
 /////////////////////////////////////////////////////////
