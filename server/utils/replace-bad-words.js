43af0b4347214439794331d6621b04c8a27b49ba
James <The-Cheese-Eater@users.noreply.github.com>
2016-07-04 15:49:37 +0100

    Adding arinerron's naughty words

--- server/utils/replace-bad-words.js
+++ server/utils/replace-bad-words.js
@@ -1,5 +1,5 @@
 const rot = require('rot')
-const badWords = '\\o(shtyl|(\\j*?)shpx(\\j*?)|s(h|i|\\*)?p?x(vat?)?|(\\j*?)fu(v|1|y)g(\\j*?)|pe(n|@|\\*)c(cre|crq|l)?|(onq|qhzo|wnpx)?(n|@)ff(u(b|0)yr|jvcr)?|(onq|qhzo|wnpx)?(n|@)efr(u(b|0)yr|jvcr)?|onfgneq|o(v|1|y|\\*)?g?pu(r?f)?|phag|phz|(tbq?)?qnz(a|z)(vg)?|qbhpur(\\j*?)|(arj)?snt(tbg|tng)?|sevt(tra|tva|tvat)?|bzst|cvff(\\j*?)|cbea|pbpx|obyybpxf|onyyfnpx|qvpx|qvpxurnq|encr|ergneq|frk|f r k|fung|fyhg|gvg|ju(b|0)er(\\j*?)|jg(s|su|u))(f|rq)?\\o' // rot13
+const badWords = '\\o(shtyl|(\\j*?)shpx(\\j*?)|s(h|i|\\*)?p?x(vat?)?|(\\j*?)fu(v|1|y)g(\\j*?)|pe(n|@|\\*)c(cre|crq|l)?|(onq|qhzo|wnpx)?(n|@)ff(u(b|0)yr|jvcr)?|(onq|qhzo|wnpx)?(n|@)efr(u(b|0)yr|jvcr)?|onfgneq|o(v|1|y|\\*)?g?pu(r?f)?|phag|phz|(tbq?)?qnz(a|z)(vg)?|qbhpur(\\j*?)|(arj)?snt(tbg|tng)?|sevt(tra|tva|tvat)?|bzst|cvff(\\j*?)|cbea|pbpx|obyybpxf|onyyfnpx|qvpx|qvpxurnq|intvan|znfgheongr|cevpx|frkl|fu vg|fuìg|sùçx|p0px|céaíf|encr|ergneq|frk|f r k|fung|fyhg|gvg|ju(b|0)er(\\j*?)|jg(s|su|u))(f|rq)?\\o' // rot13
 const badWordsRegex = new RegExp(rot(badWords, -13), 'gi')
 const hasBadWords = text => text.match(badWordsRegex)
 const replaceBadWords = (text, w='⋆⋆⋆⋆') => text.replace(badWordsRegex, w)
