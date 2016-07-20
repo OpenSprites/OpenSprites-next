92f59c2ff9608e079967183f0391ce3c7be35da5
Andrew <andrewjcolescratch@gmail.com>
2016-07-20 17:22:30 -0300

    Trying things again

--- public/assets/js/detect-ie.js
+++ public/assets/js/detect-ie.js
@@ -5,6 +5,7 @@ module.exports = function detectIE() {
   if(msie > 0) {
     // IE 10 or older => return version number
     return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10)
+    document.body.classList.add('ie-old')
   }
 
   const trident = ua.indexOf('Trident/')
@@ -12,6 +13,7 @@ module.exports = function detectIE() {
     // IE 11 => return version number
     let rv = ua.indexOf('rv:')
     return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)
+    document.body.classList.add('ie-old')
   }
 
   const edge = ua.indexOf('Edge/')
