f92c836cfceaeed3ac2384cefde03fded0734e95
MegaApuTurkUltra <apu@aputurk.tk>
2016-07-22 14:33:59 +0000

    Add youtube redirect to scratch

--- public/assets/js/leaving.js
+++ public/assets/js/leaving.js
@@ -1,4 +1,16 @@
 let safeSites = ["opensprites.org", "scratch.mit.edu", "localhost"]
+let redirects = [
+  {
+    match: 'youtube.com',
+    pathRegex: /watch\?v=([^&]+)/,
+    replace: 'https://scratch.mit.edu/discuss/youtube/{1}'
+  },
+  {
+    match: 'youtu.be',
+    pathRegex: /[^\/\?]+/,
+    replace: 'https://scratch.mit.edu/discuss/youtube/{0}'
+  }
+]
 
 document.querySelector(".os-leaving-continue").addEventListener("click", function(){
   window.open(this.dataset.siteurl)
@@ -15,11 +27,30 @@ function displayDialog(siteurl) {
   leaving.classList.add("active")
 }
 
+function doReplace(hostname, path) {
+  for(let redir of redirects) {
+    if(hostname.endsWith(redir.match)) {
+      let match = redir.pathRegex.exec(path)
+      return redir.replace.replace(/{(\d+)}/g, function(m, p1){
+        return match[parseInt(p1)]
+      })
+    }
+  }
+  return false
+}
+
 module.exports = function(siteurl){
   let l = document.createElement("a")
   l.href = siteurl
   
   let hostname = l.hostname
+  let pathname = l.pathname + l.search
+  let replace = doReplace(hostname, pathname)
+  if(replace) {
+    window.open(replace)
+    return false
+  }
+  
   for(let site of safeSites){
     if(hostname.endsWith(site))
       return true
